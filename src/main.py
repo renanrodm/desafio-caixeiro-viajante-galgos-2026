e0708fdabc7d31c85ac4e3a3b30bdeac18a7462d
Renan Martins <renanrodm@gmail.com>
2026-03-08 17:02:11 -0300

    Separa responsabilidades do arquivo e implementa main junto com a função para calcular gap

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
+from parser import load_instance
+from heuristics import vizinho_mais_proximo
+
+def calcular_gap(custo_encontrado, custo_otimo):
+    return ((custo_encontrado - custo_otimo) / custo_otimo) * 100
+
 def main():
-    pass
+    problem_data = load_instance("data/raw/brazil58.tsp")
+    cidade_inicial = 1
+    custo_otimo = 25395
+
+    rota, custo_total = vizinho_mais_proximo(problem_data, cidade_inicial)
+    gap = calcular_gap(custo_total, custo_otimo)
+
+    print(f"Instância: {problem_data['name']}")
+    print(f"Número de cidades: {problem_data['dimension']}")
+    print(f"Cidade inicial: {cidade_inicial}")
+    print(f"Custo total: {custo_total}")
+    print(f"Gap para o ótimo: {gap:.2f}%")
+    print(f"Tamanho da rota: {len(rota)}")
 
 
 if __name__ == "__main__":
